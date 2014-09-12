41080d9e1d309e36eb05745cdebb4b1d38068d75
Ben Field <ben.field@concreteplatform.com>
2014-09-12 18:02:14 +0100

    Made Linux image change to hosts

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,13 +1,15 @@
 class nagios::client {
   @@nagios_host { $hostname:
-    target  => "/etc/nagios3/conf.d/puppet/host_${fqdn}.cfg",
-    ensure  => present,
-    address => $ipaddress,
-    use     => "generic-host",
-    alias   => $hostname,
-    tag     => "${environment}",
-    parents => "${xenhost}",
-  }
+    target          => "/etc/nagios3/conf.d/puppet/host_${fqdn}.cfg",
+    ensure          => present,
+    address         => $ipaddress,
+    use             => "generic-host",
+    alias           => $hostname,
+    tag             => "${environment}",
+    parents         => "${xenhost}",
+    icon_image      => "base/linux40.png",
+    statusmap_image => "base/linux40.gd2",
+  }F
 
   @@nagios_service { "check_ping_${hostname}":
     target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
