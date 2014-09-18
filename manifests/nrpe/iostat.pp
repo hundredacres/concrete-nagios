c0b4bdd7c4a73436a506f122640566d2f6956b72
Ben Field <ben.field@concreteplatform.com>
2014-09-18 11:33:46 +0100

    Initial dynamic filesystem check

--- manifests/nrpe/iostat.pp
+++ manifests/nrpe/iostat.pp
@@ -13,58 +13,66 @@ class nagios::nrpe::iostat {
     before => File_line[check_iostat],
   }
 
-  case $::processorcount {
-    '1'     : {
-      $check = "command[check_iostat]=/usr/lib/nagios/plugins/check_iostat.sh -d xvda -W -w 999,100,200,50,80 -c 999,200,300,100,100"
+  $drive = split($::blockdevices, ",")
+  
+  nagios::nrpe::iostat::blockdevice_check{ $drive:}
+
+  # Create a definition that we can loop through
+  define nagios::nrpe::iostat::blockdevice_check {
+    case $::processorcount {
+      '1'     : {
+        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,80 -c 999,200,300,100,100"
+      }
+      '2'     : {
+        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,40 -c 999,200,300,100,50"
+      }
+      '3'     : {
+        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,27 -c 999,200,300,100,33"
+      }
+      '4'     : {
+        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,20 -c 999,200,300,100,25"
+      }
+      '5'     : {
+        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,16 -c 999,200,300,100,20"
+      }
+      '6'     : {
+        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,14 -c 999,200,300,100,16"
+      }
+      '7'     : {
+        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,12 -c 999,200,300,100,14"
+      }
+      default : {
+        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,10 -c 999,200,300,100,13"
+      }
     }
-    '2'     : {
-      $check = "command[check_iostat]=/usr/lib/nagios/plugins/check_iostat.sh -d xvda -W -w 999,100,200,50,40 -c 999,200,300,100,50"
+
+    file_line { "check_iostat":
+      line   => $check,
+      path   => "/etc/nagios/nrpe_local.cfg",
+      match  => "command\[check_iostat_$name\]",
+      ensure => present,
+      notify => Service[nrpe],
     }
-    '3'     : {
-      $check = "command[check_iostat]=/usr/lib/nagios/plugins/check_iostat.sh -d xvda -W -w 999,100,200,50,27 -c 999,200,300,100,33"
+
+    case $::environment {
+      'production'  : { $service = "generic-service-excluding-pagerduty" }
+      'testing'     : { $service = "generic-service" }
+      'development' : { $service = "generic-service-excluding-pagerduty" }
+      default       : { $service = "generic-service-excluding-pagerduty" }
     }
-    '4'     : {
-      $check = "command[check_iostat]=/usr/lib/nagios/plugins/check_iostat.sh -d xvda -W -w 999,100,200,50,20 -c 999,200,300,100,25"
+
+    @@nagios_service { "check_iostat_${hostname}_$name":
+      check_command         => "check_nrpe_1arg_longtimeout!check_iostat_$name",
+      use                   => $service,
+      host_name             => $hostname,
+      target                => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
+      service_description   => "${hostname}_check_iostat_$name",
+      tag                   => "${environment}",
+      notifications_enabled => 0,
     }
-    '5'     : {
-      $check = "command[check_iostat]=/usr/lib/nagios/plugins/check_iostat.sh -d xvda -W -w 999,100,200,50,16 -c 999,200,300,100,20"
-    }
-    '6'     : {
-      $check = "command[check_iostat]=/usr/lib/nagios/plugins/check_iostat.sh -d xvda -W -w 999,100,200,50,14 -c 999,200,300,100,16"
-    }
-    '7'     : {
-      $check = "command[check_iostat]=/usr/lib/nagios/plugins/check_iostat.sh -d xvda -W -w 999,100,200,50,12 -c 999,200,300,100,14"
-    }
-    default : {
-      $check = "command[check_iostat]=/usr/lib/nagios/plugins/check_iostat.sh -d xvda -W -w 999,100,200,50,10 -c 999,200,300,100,13"
-    }
+
   }
 
-  file_line { "check_iostat":
-    line   => $check,
-    path   => "/etc/nagios/nrpe_local.cfg",
-    match  => "command\[check_iostat\]",
-    ensure => present,
-    notify => Service[nrpe],
-  }
-
-  case $::environment {
-    'production' : { $service = "generic-service-excluding-pagerduty" }
-    'testing'     : { $service = "generic-service" }
-    'development' : { $service = "generic-service-excluding-pagerduty" }
-    default       : { $service = "generic-service-excluding-pagerduty" }
-  }
-
-  @@nagios_service { "check_iostat_${hostname}":
-    check_command       => "check_nrpe_1arg_longtimeout!check_iostat",
-    use                 => $service,
-    host_name           => $hostname,
-    target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
-    service_description => "${hostname}_check_iostat",
-    tag                 => "${environment}",
-    notifications_enabled => 0,
-  }
-
-  @basic_server::motd::register { 'Nagios Diskspeed Check': }
+  @basic_server::motd::register { "Nagios Diskspeed Check $name": }
 
 }
