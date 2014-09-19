0eaa71303e1c1759dc9fde8e9e8105f938deab17
Ben Field <ben.field@concreteplatform.com>
2014-09-19 16:19:07 +0100

    Corrected template name

--- manifests/server/iostat.pp
+++ manifests/server/iostat.pp
@@ -1,14 +1,10 @@
 class nagios::server::iostat {
-  
   require nagios::server::config
   include nagios::server::service
-  
-    Datacat_fragment <<| tag == "iostat${environment}" |>> {
-    }
-    
-    datacat { '/tmp/test.cfg':
-			template => "${module_name}/hostgroups.cfg.erb",
-}
-    
-    
+
+  Datacat_fragment <<| tag == "iostat${environment}" |>> {
+  }
+
+  datacat { '/tmp/test.cfg': template => "nagios/servicegroup_iostat.erb", }
+
 }
