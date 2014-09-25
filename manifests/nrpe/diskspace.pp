9fd0f7607e48bd1657c15eae0a5e371cd5573b37
Ben Field <ben.field@concreteplatform.com>
2014-09-25 12:22:41 +0100

    Renamed command

--- manifests/nrpe/diskspace.pp
+++ manifests/nrpe/diskspace.pp
@@ -4,10 +4,10 @@ class nagios::nrpe::diskspace {
   require nagios::nrpe::config
   include nagios::nrpe::service
 
-  file_line { "check_disk":
-    line   => "command[check_disk]=/usr/lib/nagios/plugins/check_disk -w 20% -c 10% -p /",
+  file_line { "check_root_diskspace":
+    line   => "command[check_root_diskspace]=/usr/lib/nagios/plugins/check_disk -w 20% -c 10% -p /",
     path   => "/etc/nagios/nrpe_local.cfg",
-    match  => "command\[check_disk\]",
+    match  => "command\[check_root_diskspace\]",
     ensure => present,
     before => File_line[check_disk_default],
   }
@@ -21,7 +21,7 @@ class nagios::nrpe::diskspace {
   }
 
   @@nagios_service { "check_sysvol_space_${hostname}":
-    check_command       => "check_nrpe_1arg!check_disk",
+    check_command       => "check_nrpe_1arg!check_root_diskspace",
     use                 => "generic-service",
     host_name           => $hostname,
     target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
