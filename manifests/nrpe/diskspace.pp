96951013c1f684e147d84ff451c2c8b00a413a7d
Ben Field <ben.field@concreteplatform.com>
2014-09-29 10:58:49 +0100

    Removed checks for xvdd (disk drive on xen hosts). May want to paramterise this out later

--- manifests/nrpe/diskspace.pp
+++ manifests/nrpe/diskspace.pp
@@ -19,34 +19,36 @@ class nagios::nrpe::diskspace {
   nagios::nrpe::diskspace::blockdevice_check { $drive: require => File_Line["check_disk_default"], }
 
   define nagios::nrpe::diskspace::blockdevice_check {
-    file_line { "check_${name}_diskspace":
-      line   => "command[check_${name}_diskspace]=/usr/lib/nagios/plugins/check_disk -w 20% -c 10% -x ${name}",
-      path   => "/etc/nagios/nrpe_local.cfg",
-      match  => "command\[check_${name}_diskspace\]",
-      ensure => present,
-      notify => Service[nrpe],
+    if $name != "xvdd" {
+      file_line { "check_${name}_diskspace":
+        line   => "command[check_${name}_diskspace]=/usr/lib/nagios/plugins/check_disk -w 20% -c 10% -x ${name}",
+        path   => "/etc/nagios/nrpe_local.cfg",
+        match  => "command\[check_${name}_diskspace\]",
+        ensure => present,
+        notify => Service[nrpe],
+      }
+
+      # For neatness :
+
+      if $name == "xvda" {
+        $drive = "sysvol"
+      } else {
+        $drive = $name
+      }
+
+      @@nagios_service { "check_${drive}_space_${hostname}":
+        check_command       => "check_nrpe_1arg!check_${name}_diskspace",
+        use                 => "generic-service",
+        host_name           => $hostname,
+        target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
+        service_description => "${hostname}_check_${drive}_space",
+        tag                 => "${environment}",
+      }
+
+      @basic_server::motd::register { "Nagios Diskspace Check $name": }
+
     }
-
-    # For neatness :
-
-    if $name == "xvda" {
-      $drive = "sysvol"
-    } else {
-      $drive = $name
-    }
-
-    @@nagios_service { "check_${drive}_space_${hostname}":
-      check_command       => "check_nrpe_1arg!check_${name}_diskspace",
-      use                 => "generic-service",
-      host_name           => $hostname,
-      target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
-      service_description => "${hostname}_check_${drive}_space",
-      tag                 => "${environment}",
-    }
-
-    @basic_server::motd::register { "Nagios Diskspace Check $name": }
 
   }
-
 }
 
--- manifests/nrpe/iostat.pp
+++ manifests/nrpe/iostat.pp
@@ -31,7 +31,7 @@ class nagios::nrpe::iostat {
     ,
     tag    => "iostat_${environment}",
   }
-	
+
   $drive = split($::blockdevices, ",")
 
   nagios::nrpe::iostat::blockdevice_check { $drive: require => File["check_iostat.sh"], }
@@ -39,61 +39,63 @@ class nagios::nrpe::iostat {
   # Create a definition that we can loop through
   # May need to review what we consider to be actionable levels for meaningful alerting on these... --Justin
   define nagios::nrpe::iostat::blockdevice_check {
-    case $::processorcount {
-      '1'     : {
-        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,80 -c 999,200,300,100,100"
+    if $name != "xvdd" {
+      case $::processorcount {
+        '1'     : {
+          $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,80 -c 999,200,300,100,100"
+        }
+        '2'     : {
+          $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,40 -c 999,200,300,100,50"
+        }
+        '3'     : {
+          $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,27 -c 999,200,300,100,33"
+        }
+        '4'     : {
+          $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,20 -c 999,200,300,100,25"
+        }
+        '5'     : {
+          $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,16 -c 999,200,300,100,20"
+        }
+        '6'     : {
+          $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,14 -c 999,200,300,100,16"
+        }
+        '7'     : {
+          $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,12 -c 999,200,300,100,14"
+        }
+        default : {
+          $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,10 -c 999,200,300,100,13"
+        }
       }
-      '2'     : {
-        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,40 -c 999,200,300,100,50"
+
+      file_line { "check_iostat_$name":
+        line   => $check,
+        path   => "/etc/nagios/nrpe_local.cfg",
+        match  => "command\[check_iostat_$name\]",
+        ensure => present,
+        notify => Service[nrpe],
       }
-      '3'     : {
-        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,27 -c 999,200,300,100,33"
+
+      case $::environment {
+        'production'  : { $service = "generic-service-excluding-pagerduty" }
+        'testing'     : { $service = "generic-service" }
+        'development' : { $service = "generic-service-excluding-pagerduty" }
+        default       : { $service = "generic-service-excluding-pagerduty" }
       }
-      '4'     : {
-        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,20 -c 999,200,300,100,25"
+
+      @@nagios_service { "check_iostat_${hostname}_$name":
+        check_command       => "check_nrpe_1arg_longtimeout!check_iostat_$name",
+        use                 => $service,
+        host_name           => $hostname,
+        target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
+        service_description => "${hostname}_check_iostat_$name",
+        tag                 => "${environment}",
+        servicegroups       => "servicegroup_iostat_${xenhost}",
+      # notifications_enabled => 0,
       }
-      '5'     : {
-        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,16 -c 999,200,300,100,20"
-      }
-      '6'     : {
-        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,14 -c 999,200,300,100,16"
-      }
-      '7'     : {
-        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,12 -c 999,200,300,100,14"
-      }
-      default : {
-        $check = "command[check_iostat_$name]=/usr/lib/nagios/plugins/check_iostat.sh -d $name -W -w 999,100,200,50,10 -c 999,200,300,100,13"
-      }
+
+      @basic_server::motd::register { "Nagios Diskspeed Check $name": }
+
     }
-
-    file_line { "check_iostat_$name":
-      line   => $check,
-      path   => "/etc/nagios/nrpe_local.cfg",
-      match  => "command\[check_iostat_$name\]",
-      ensure => present,
-      notify => Service[nrpe],
-    }
-
-    case $::environment {
-      'production'  : { $service = "generic-service-excluding-pagerduty" }
-      'testing'     : { $service = "generic-service" }
-      'development' : { $service = "generic-service-excluding-pagerduty" }
-      default       : { $service = "generic-service-excluding-pagerduty" }
-    }
-
-    @@nagios_service { "check_iostat_${hostname}_$name":
-      check_command       => "check_nrpe_1arg_longtimeout!check_iostat_$name",
-      use                 => $service,
-      host_name           => $hostname,
-      target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
-      service_description => "${hostname}_check_iostat_$name",
-      tag                 => "${environment}",
-      servicegroups       => "servicegroup_iostat_${xenhost}",
-    # notifications_enabled => 0,
-    }
-
-    @basic_server::motd::register { "Nagios Diskspeed Check $name": }
-
   }
 
 }
