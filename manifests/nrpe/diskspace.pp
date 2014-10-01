c0ae796a3f87cfca51d26219e243de84e4e76a6b
Ben Field <ben.field@concreteplatform.com>
2014-10-01 10:46:07 +0100

    Added debug statement

--- manifests/nrpe/diskspace.pp
+++ manifests/nrpe/diskspace.pp
@@ -16,23 +16,25 @@ class nagios::nrpe::diskspace ($nagios_service = $nagios::params::nagios_service
 
   $drive = split($::blockdevices, ",")
 
-  nagios::nrpe::diskspace::blockdevice_check { $drive: require => File_Line["check_disk_default"], nagios_service => $nagios_service }
+  nagios::nrpe::diskspace::blockdevice_check { $drive:
+    require        => File_Line["check_disk_default"],
+    nagios_service => $nagios_service
+  }
 
-  define nagios::nrpe::diskspace::blockdevice_check ($nagios_service = "generic_service"){
+  define nagios::nrpe::diskspace::blockdevice_check ($nagios_service = "generic_service") {
     if $name != "xvdd" and $name != "sr0" {
-      
-      #Going to have a different check for very large disks ( gt than 100GB)
-      
-      $size =getvar("blockdevice_${name}_size")
-      
-      if $size > 100*1024*1024*1024 {
-      	$warning = "10"
-				$critical = "5"
+      # Going to have a different check for very large disks ( gt than 100GB)
+
+      $size = getvar("blockdevice_${name}_size")
+
+      if $size > 100 * 1024 * 1024 * 1024 {
+        $warning = "10"
+        $critical = "5"
       } else {
         $warning = "20"
         $critical = "10"
-      } 
-      
+      }
+
       file_line { "check_${name}_diskspace":
         line   => "command[check_${name}_diskspace]=/usr/lib/nagios/plugins/check_disk -E -w ${warning}% -c ${critical}% -R /dev/${name}*",
         path   => "/etc/nagios/nrpe_local.cfg",
@@ -62,6 +64,27 @@ class nagios::nrpe::diskspace ($nagios_service = $nagios::params::nagios_service
 
     }
 
+  }
+
+  if ${lvm}== "true" or ${hostname} == "bendev" {
+    $excludedDrives = join(prefix(${drive}, "-I "), " ")
+
+    file_line { "check_lvm_diskspace":
+      line   => "command[check_lvm_diskspace]=/usr/lib/nagios/plugins/check_disk -w 20% -c 10% -p / ${excludedDrives}",
+      path   => "/etc/nagios/nrpe_local.cfg",
+      match  => "command\[check_lvm_diskspace\]",
+      ensure => present,
+      notify => Service[nrpe],
+    }
+
+    @@nagios_service { "check_lvm_space_${hostname}":
+      check_command       => "check_nrpe_1arg!check_lvm_diskspace",
+      use                 => "${nagios_service}",
+      host_name           => $hostname,
+      target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
+      service_description => "${hostname}_check_lvm_space",
+      tag                 => "${environment}",
+    }
   }
 }
 
