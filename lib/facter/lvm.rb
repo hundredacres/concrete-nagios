67e9a243ae333af544039185d88f9cc1fe0ad433
Ben Field <ben.field@concreteplatform.com>
2014-10-01 10:49:40 +0100

    Moved lvm fact

---
+++ lib/facter/lvm.rb
@@ -0,0 +1,13 @@
+# lvm.rb
+
+Facter.add('lvm') do
+  setcode do
+    virtualtype = Facter.value(:virtual)
+    lvscan = Facter::Util::Resolution.exec('/sbin/lvscan 2>/dev/null | grep ACTIVE')
+   if lvscan != "" and !lvscan.nil?
+      'true'
+    else
+      'false'
+    end
+  end
+end
