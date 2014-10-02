23698fd8fc4a52f8467faf131100822270a5943e
Ben Field <ben.field@concreteplatform.com>
2014-10-02 11:15:22 +0100

    Added override for nagios parent

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,27 +1,38 @@
-class nagios::client (
-  $nagios_service = $nagios::params::nagios_service
-  ) inherits nagios::params {
-    
-    #Gonna take in a nagios_parent variable as an override
-    
-    if $::nagios_parent != "" {
-      $parent = $::nagios_parent
-    } else {
-      $parent = $xenhost
-    } 
-    
-  @@nagios_host { $hostname :
-    target          => "/etc/nagios3/conf.d/puppet/host_${fqdn}.cfg",
-    ensure          => present,
-    address         => $ipaddress_eth0,
-    use             => "generic-host",
-    alias           => $hostname,
-    tag             => "${environment}",
-    if $parent != "" {
-    parents         => "${parent}",
+class nagios::client ($nagios_service = $nagios::params::nagios_service) inherits nagios::params {
+  # Gonna take in a nagios_parent variable as an override
+
+  if $::nagios_parent != "" {
+    $parent = $::nagios_parent
+  } else {
+    $parent = $xenhost
+  }
+
+  # The not hugely neat way, need to refactor this:
+
+  if $parent != "" {
+    @@nagios_host { $hostname:
+      target          => "/etc/nagios3/conf.d/puppet/host_${fqdn}.cfg",
+      ensure          => present,
+      address         => $ipaddress_eth0,
+      use             => "generic-host",
+      alias           => $hostname,
+      tag             => "${environment}",
+      parents         => "${parent}",
+      icon_image      => "base/linux40.png",
+      statusmap_image => "base/linux40.gd2",
     }
-    icon_image      => "base/linux40.png",
-    statusmap_image => "base/linux40.gd2",
+  } else {
+    @@nagios_host { $hostname:
+      target          => "/etc/nagios3/conf.d/puppet/host_${fqdn}.cfg",
+      ensure          => present,
+      address         => $ipaddress_eth0,
+      use             => "generic-host",
+      alias           => $hostname,
+      tag             => "${environment}",
+      parents         => "${parent}",
+      icon_image      => "base/linux40.png",
+      statusmap_image => "base/linux40.gd2",
+    }
   }
 
   @@nagios_service { "check_ping_${hostname}":
