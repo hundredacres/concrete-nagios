981e91a8f3def39253eb45df10cae3bbb4f785c7
Ben Field <ben.field@concreteplatform.com>
2014-10-06 09:57:29 +0100

    Changed total procs number default

--- manifests/nrpe/total_procs.pp
+++ manifests/nrpe/total_procs.pp
@@ -1,6 +1,4 @@
-class nagios::nrpe::total_procs (
-  $nagios_service = $nagios::params::nagios_service
-  ) inherits nagios::params {
+class nagios::nrpe::total_procs ($nagios_service = $nagios::params::nagios_service) inherits nagios::params {
   require nagios::nrpe::config
   include nagios::nrpe::service
 
@@ -11,6 +9,22 @@ class nagios::nrpe::total_procs (
     target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
     service_description => "${hostname}_check_total_procs",
     tag                 => "${environment}",
+  }
+
+  file_line { "check_total_procs_default":
+    line   => "command[check_total_procs]=/usr/lib/nagios/plugins/check_procs -w 150 -c 200",
+    path   => "/etc/nagios/nrpe.cfg",
+    match  => "command\[check_total_procs\]",
+    ensure => absent,
+    notify => Service[nrpe],
+  }
+
+  file_line { "check_total_procs":
+    line   => "command[check_total_procs]=/usr/lib/nagios/plugins/check_procs -w 200 -c 250",
+    path   => "/etc/nagios/nrpe_local.cfg",
+    match  => "command\[check_total_procs\]",
+    ensure => present,
+    notify => Service[nrpe],
   }
 
   @basic_server::motd::register { 'Nagios Total Processes Check': }
