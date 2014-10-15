2964857d29c375c62669df9853c9f6a3005247c0
Justin Miller <justin.miller@office.concreteplatform.net>
2014-10-15 09:58:21 +0100

    added reboot.pp checks for umm, reboots required.. --Justin

---
+++ manifests/nrpe/reboot.pp
@@ -0,0 +1,37 @@
+# Checks if a host needs to be rebooted as a result of updates.  The script checks for
+# /var/run/reboot-required and raises a warning if present.
+
+class nagios::nrpe::reboot ($nagios_service = $nagios::params::nagios_service) inherits nagios::params {
+  require nagios::nrpe::config
+  include nagios::nrpe::service
+
+  file { "check_mem.sh":
+    path   => "/usr/lib/nagios/plugins/check_reboot.sh",
+    source => "puppet:///modules/nagios/check_reboot.sh",
+    owner  => root,
+    group  => root,
+    mode   => "0755",
+    ensure => present,
+    before => File_line[check_reboot],
+  }
+
+  file_line { "check_reboot":
+    line   => "command[check_reboot]=/usr/lib/nagios/plugins/check_mem.sh -w 85 -c 95",
+    path   => "/etc/nagios/nrpe_local.cfg",
+    match  => "command\[check_reboot\]",
+    ensure => present,
+    notify => Service[nrpe],
+  }
+
+  @@nagios_service { "check_reboot_${hostname}":
+    check_command       => "check_nrpe_1arg!check_reboot",
+    use                 => "${nagios_service}",
+    host_name           => $hostname,
+    target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
+    service_description => "${hostname}_check_reboot",
+    tag                 => "${environment}",
+  }
+
+  @motd::register { 'Nagios Reboot Check': }
+
+}
