dfe5d39d2278e0cedcff5eb8ecb7a61175e1758a
Ben Field <ben.field@concreteplatform.com>
2014-10-16 17:57:16 +0100

    Fixed check_https_nonroot command

--- manifests/server/clean.pp
+++ manifests/server/clean.pp
@@ -8,9 +8,9 @@ class nagios::server::clean {
   }
   
   nagios_command { 'Check HTTPS nonroot':
-    command_name => 'check_nrpe_1arg_longtimeout',
+    command_name => 'check_https_nonroot',
     ensure       => 'present',
-    command_line => 'check_http -S -I $HOSTADDRESS$ -H $ARG1$ -u $ARG2$',
+    command_line => '/usr/lib/nagios/plugins/check_http -S -I $HOSTADDRESS$ -H $ARG1$ -u $ARG2$',
     target       => "/etc/nagios3/conf.d/puppet/nagios_commands.cfg",
     notify       => Exec["rechmod"],
   } 
