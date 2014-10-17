f80869afac17bf10fb86517c1f5b2e6ef24789dc
Ben Field <ben.field@concreteplatform.com>
2014-10-17 13:35:35 +0100

    Added new command for use with workspaces api

--- manifests/server/clean.pp
+++ manifests/server/clean.pp
@@ -6,13 +6,21 @@ class nagios::server::clean {
     target       => "/etc/nagios3/conf.d/puppet/nagios_commands.cfg",
     notify       => Exec["rechmod"],
   }
-  
+
   nagios_command { 'Check HTTPS nonroot':
     command_name => 'check_https_nonroot',
     ensure       => 'present',
     command_line => '/usr/lib/nagios/plugins/check_http -S -I $HOSTADDRESS$ -H $ARG1$ -u $ARG2$',
     target       => "/etc/nagios3/conf.d/puppet/nagios_commands.cfg",
     notify       => Exec["rechmod"],
-  } 
+  }
+
+  nagios_command { 'Check HTTP nonroot custom port':
+    command_name => 'check_https_nonroot_custom_port',
+    ensure       => 'present',
+    command_line => '/usr/lib/nagios/plugins/check_http -I $HOSTADDRESS$ -H $ARG1$ -u $ARG2$ -p $ARG3$',
+    target       => "/etc/nagios3/conf.d/puppet/nagios_commands.cfg",
+    notify       => Exec["rechmod"],
+  }
 
 }
