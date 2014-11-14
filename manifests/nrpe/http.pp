dea888468f9dd5a7a37e0ef64b214f5fb4b36467
Ben Field <ben.field@concreteplatform.com>
2014-11-14 12:02:58 +0000

    Added generic http check

---
+++ manifests/nrpe/http.pp
@@ -0,0 +1,45 @@
+define nagios::nrpe::http ($health_check_uri = "/", $port = "80", $has_parent = false, $parent_service = "/", $ssl = false,) {
+  require nagios::nrpe::config
+  include nagios::nrpe::service
+  include nagios::params
+  $nagios_service = $::nagios::params::nagios_service
+
+  if $ssl == true {
+    $protocol = "HTTPS"
+    $command = "check_https_nonroot_custom_port"
+  } else {
+    $protocol = "HTTP"
+    $command = "check_http_nonroot_custom_port"
+  }
+
+  # This will use the name as the hostname to check ( this is really important with ssl! Can add a parameter if we thing of a
+  # usecase
+
+  @@nagios_service { "check_${name}_${protocol}_on_${hostname}":
+    check_command       => "${command}!${hostname}!${health_check_uri}!${port}",
+    use                 => "${nagios_service}",
+    host_name           => $hostname,
+    target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
+    service_description => "${hostname}_check_workspacesapi_http",
+    tag                 => "${environment}",
+  }
+
+  if $has_parent == true {
+    @@nagios_servicedependency { "${name}_on_${hostname}_depencency_process":
+      dependent_host_name           => $hostname,
+      dependent_service_description => "${name}",
+      host_name => $hostname,
+      service_description           => "${parent_service}",
+      execution_failure_criteria    => "c",
+      notification_failure_criteria => "c",
+      target    => "/etc/nagios3/conf.d/puppet/service_dependencies_${fqdn}.cfg",
+      tag       => "${environment}",
+    }
+  }
+
+  if $has_parent == true {
+    @motd::register { "Nagios HTTP Check for ${name} and service dependency on ${parent_service}": }
+  } else {
+    @motd::register { "Nagios HTTP Check for ${name}": }
+  }
+}
--- manifests/server/clean.pp
+++ manifests/server/clean.pp
@@ -7,18 +7,18 @@ class nagios::server::clean {
     notify       => Exec["rechmod"],
   }
 
-  nagios_command { 'Check HTTPS nonroot':
-    command_name => 'check_https_nonroot',
-    ensure       => 'present',
-    command_line => '/usr/lib/nagios/plugins/check_http -S -I $HOSTADDRESS$ -H $ARG1$ -u $ARG2$',
-    target       => "/etc/nagios3/conf.d/puppet/nagios_commands.cfg",
-    notify       => Exec["rechmod"],
-  }
-
   nagios_command { 'Check HTTP nonroot custom port':
     command_name => 'check_http_nonroot_custom_port',
     ensure       => 'present',
     command_line => '/usr/lib/nagios/plugins/check_http -I $HOSTADDRESS$ -H $ARG1$ -u $ARG2$ -p $ARG3$',
+    target       => "/etc/nagios3/conf.d/puppet/nagios_commands.cfg",
+    notify       => Exec["rechmod"],
+  }
+
+  nagios_command { 'Check HTTPS nonroot custom port':
+    command_name => 'check_https_nonroot',
+    ensure       => 'present',
+    command_line => '/usr/lib/nagios/plugins/check_http -S -I $HOSTADDRESS$ -H $ARG1$ -u $ARG2$ -p $ARG3$',
     target       => "/etc/nagios3/conf.d/puppet/nagios_commands.cfg",
     notify       => Exec["rechmod"],
   }
