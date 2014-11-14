7bbdbacb663a4f1b40b4b14538debdfb880df04c
Ben Field <ben.field@concreteplatform.com>
2014-11-14 14:43:27 +0000

    Missing command definition

--- manifests/server/clean.pp
+++ manifests/server/clean.pp
@@ -16,7 +16,7 @@ class nagios::server::clean {
   }
 
   nagios_command { 'Check HTTPS nonroot custom port':
-    command_name => 'check_https_nonroot',
+    command_name => 'check_http_nonroot_custom_port',
     ensure       => 'present',
     command_line => '/usr/lib/nagios/plugins/check_http -S -I $HOSTADDRESS$ -H $ARG1$ -u $ARG2$ -p $ARG3$',
     target       => "/etc/nagios3/conf.d/puppet/nagios_commands.cfg",
