b14a0a38578edf5e18b64d70e0970a4ca3f82aa8
Ben Field <ben.field@concreteplatform.com>
2014-11-17 15:37:19 +0000

    Removed now unnecessary files

--- manifests/server/nrpe.pp
+++ manifests/server/nrpe.pp
@@ -7,20 +7,6 @@ class nagios::server::nrpe {
     notify => Service[nagios3],
   }
 
-  #       Automatically added in ubuntu install
-  #
-  #               nagios_command { 'Check Nrpe':
-  #                       command_name => 'check_nrpe',
-  #                       ensure       => 'present',
-  #                       command_line => '/usr/lib64/nagios/plugins/check_nrpe -H $HOSTADDRESS$ -c $ARG1$',
-  #                       target => "/etc/nagios3/conf.d/nagios_commands.cfg",
-  #               }
-
-  class clean {
-
-
-  }
-
   @motd::register { 'Nagios NRPE Server': }
 
 }
