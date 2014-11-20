e91de16c24409a0bf49e7543ae810a340cce839e
Ben Field <ben.field@concreteplatform.com>
2014-11-20 12:10:05 +0000

    Added check_file_count script and define manifest

---
+++ files/check_file_count.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+# ----------check_file_count.sh-----------
+# This is going to check the number of files in a specific directory is below certain thresholds 
+# and return results in a nagios compatible format.
+#
+# Version 0.01 - Nov/2014
+# by Ben Field / ben.field@concreteplatform.com
+
+# Exit codes
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+
+function help {
+	echo -e "
+	This is going to check the number of files in a specific directory is below certain thresholds 
+	and return results in a nagios compatible format.
+		
+	Usage:
+
+	-d = Directory to be checked. Example: \"-d /tmp/\". Required parameter.
+
+	-w,-c = Warning and critical levels respectively. Required parameter.
+		
+	-r = recursive mode.
+
+	-h = This help
+	"
+	exit -1
+}
+
+# ----------PARAMETER INPUT AND TESTING-----------
+
+DIRECTORYFLAG=false
+WARNINGFLAG=false
+DIRECTORYFLAG=false
+ARGUMENTFLAG=false
+RECURSE=""
+
+while getopts "d:w:c:r" OPT; do
+	case $OPT in
+		"d") DIRECTORY=$OPTARG
+		DIRECTORYFLAG=true
+		;;
+		"w") WARNING=$OPTARG
+		WARNINGFLAG=true
+		;;
+		"c") CRITICAL=$OPTARG
+		CRITICALFLAG=true
+		;;
+		"r") RECURSE="-R"
+		;;
+		"h") echo "help:" && help
+		;;
+		\?) echo "UNKNOWN - Invalid option: -$OPT" >&2
+		ARGUMENTFLAG=true
+		;;
+		:) echo "UNKNOWN - Option -$OPTARG requires an argument" >&2
+		ARGUMENTFLAG=true
+		;;
+	esac
+done		
+
+#Checks to see if any arguments are missing:
+if ! $WARNINGFLAG; then
+	echo "UNKNOWN - Warning level parameter required" >&2
+	ARGUMENTFLAG=true
+fi
+if ! $CRITICALFLAG; then
+	echo "UNKNOWN - Critical level parameter required" >&2
+	ARGUMENTFLAG=true
+fi
+if ! $IPFLAG; then
+	echo "UNKNOWN - Directory parameter required" >&2
+	ARGUMENTFLAG=true
+fi
+
+#Checks for sane Warning/Critical levels
+if [ $WARNING -gt $CRITICAL ]; then
+	echo "UNKNOWN - Warning level should not be higher than Critical level" >&2
+	ARGUMENTFLAG=true
+fi
+
+#Checks to see if valid directory
+if [ ! -d $DIRECTORY ]; then
+	echo "UNKNOWN - $DIRECTORY is not a valid directory" >&2
+	ARGUMENTFLAG=true
+fi
+
+if $ARGUMENTFLAG; then
+	exit $STATE_UNKNOWN
+fi
+
+# ----------FILE COUNT CALCULATION-----------
+
+FILE_COUNT=`ls -la $RECURSE $DIRECTORY | grep -i ^- | wc -l`
+
+# ----------FILE COUNT TEST AND RETURN TO NAGIOS-----------
+
+if [ $FILE_COUNT -ge $CRITICAL ]; then
+	echo "CRITICAL - $FILE_COUNT files in $DIRECTORY"
+	exit $STATE_CRITICAL
+elif [ $FILE_COUNT -ge $WARNING ]; then
+	echo "WARNING - $FILE_COUNT files in $DIRECTORY"
+	exit $STATE_WARNING
+else
+	echo "OK - $FILE_COUNT files in $DIRECTORY"
+	exit $STATE_OK
+fi
---
+++ manifests/nrpe/file_count.pp
@@ -0,0 +1,48 @@
+#Gonna make a generic check that can use the check_file_count script
+
+define nagios::nrpe::file_count ($directory         = $name,
+  $warning         = "2",
+  $critical        = "1",
+  $recurse          = true
+  ) {
+  require nagios::nrpe::config
+  include nagios::nrpe::service
+  include nagios::params
+  $nagios_service = $::nagios::params::nagios_service
+
+  file { "check_file_count.sh":
+    path   => "/usr/lib/nagios/plugins/check_file_count.sh",
+    source => "puppet:///modules/nagios/check_file_count.sh",
+    owner  => nagios,
+    group  => nagios,
+    mode   => "0755",
+    ensure => present,
+    before => File_line[check_file_count],
+  }
+
+	if $recurse == true {
+	  $command = "command[check_file_count_${directory}]=/usr/lib/nagios/plugins/check_file_count.sh -w ${warning} -c ${critical} -r -d ${directory}"
+	} else {
+	  $command = "command[check_file_count_${directory}]=/usr/lib/nagios/plugins/check_file_count.sh -w ${warning} -c ${critical} -d ${directory}"
+	}
+
+  file_line { "check_file_count_${directory}":
+    line   => $command,
+    path   => "/etc/nagios/nrpe_local.cfg",
+    match  => "command\[check_file_count_${directory}\]",
+    ensure => present,
+    notify => Service[nrpe],
+  }
+
+  @@nagios_service { "check_file_count_${directory}_on_${hostname}":
+      check_command       => "check_nrpe_1arg!check_file_count_${directory}",
+      use                 => "${nagios_service}",
+      host_name           => $hostname,
+      target              => "/etc/nagios3/conf.d/puppet/service_${fqdn}.cfg",
+      service_description => "${hostname}_check_file_count_${directory}",
+      tag                 => "${environment}",
+  }
+
+    @motd::register { "Nagios File Count Check on ${directory}": }
+
+}
