c2e592635a592743bae01992e39c781a1196cd3f
Ben Field <ben.field@concreteplatform.com>
2014-12-11 10:35:58 +0000

    Removed unnecessary line from fact

--- lib/facter/lvm.rb
+++ lib/facter/lvm.rb
@@ -2,9 +2,8 @@
 
 Facter.add('lvm') do
   setcode do
-    virtualtype = Facter.value(:virtual)
     lvscan = Facter::Util::Resolution.exec('/sbin/lvscan 2>/dev/null | grep ACTIVE')
-   if lvscan != "" and !lvscan.nil?
+    if lvscan != "" and !lvscan.nil?
       true
     else
       false
