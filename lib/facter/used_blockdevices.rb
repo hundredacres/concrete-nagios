54cc1006b6a276255e12d4fb8941aad0ee1d058c
Ben Field <ben.field@concreteplatform.com>
2015-01-09 11:58:52 +0000

    Corrected debugging

--- lib/facter/used_blockdevices.rb
+++ lib/facter/used_blockdevices.rb
@@ -12,7 +12,7 @@ Facter.add('used_blockdevices') do
         puts blockdevice
       end
     end
-    put used_blockdevices = used_blockdevices_array.map {|element|
+    puts used_blockdevices = used_blockdevices_array.map {|element|
       "#{element}"
     }.join(',')
     used_blockdevices = used_blockdevices_array.map {|element|
