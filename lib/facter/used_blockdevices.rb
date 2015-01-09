1ca4a02900734fe740812bf70245033c6c050401
Ben Field <ben.field@concreteplatform.com>
2015-01-09 12:07:38 +0000

    Removed debugging

--- lib/facter/used_blockdevices.rb
+++ lib/facter/used_blockdevices.rb
@@ -3,19 +3,13 @@
 Facter.add('used_blockdevices') do
   setcode do
     blockdevices = Facter.value(:blockdevices)
-    puts blockdevices
     used_blockdevices_array = Array.new
     blockdevice_array = blockdevices.split(',')
     blockdevice_array.each do |blockdevice|
-      puts blockdevice
       if Facter::Util::Resolution.exec("/bin/df -h | /bin/grep #{blockdevice}") != ''
         used_blockdevices_array.push(blockdevice)
-        puts blockdevice
       end
     end
-    puts used_blockdevices = used_blockdevices_array.map {|element|
-      "#{element}"
-    }.join(',')
     used_blockdevices = used_blockdevices_array.map {|element|
       "#{element}"
     }.join(',')
