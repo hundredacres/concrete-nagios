aa06fc935256a5f249824013d2218c4f5c51f732
Ben Field <ben.field@concreteplatform.com>
2015-01-20 11:52:26 +0000

    Fixed mixed up manifest names

--- manifests/nrpe/kernel_leak.pp
+++ manifests/nrpe/kernel_leak.pp
@@ -1,6 +1,7 @@
-# == Class: nagios::nrpe::lowmemory
+# == Class: nagios::nrpe::kernel_leak
 #
-# Uses a simple lowmemory check. Will warn if less than 3% lowmemory, critical on 1%.
+# Uses a simple kernel leak check. Will warn if less than 3% lowmemory, critical
+# on 1% AND will warn if more than 8000000 objects, critical on 10000000
 #
 # It will deploy the check, add the command and then create the service on the
 # nagios server
@@ -14,40 +15,40 @@
 # === Authors
 #
 # Ben Field <ben.field@concreteplatform.com
-class nagios::nrpe::lowmemory {
+class nagios::nrpe::kernel_leak {
   require nagios::nrpe::config
   include nagios::nrpe::service
   include nagios::params
 
   $nagios_service = $::nagios::params::nagios_service
 
-  file { 'check_lowmemory.sh':
+  file { 'check_kernel_leak.sh':
     ensure => present,
-    path   => '/usr/lib/nagios/plugins/check_lowmemory.sh',
-    source => 'puppet:///modules/nagios/check_lowmemory.sh',
+    path   => '/usr/lib/nagios/plugins/check_kernel_leak.sh',
+    source => 'puppet:///modules/nagios/check_kernel_leak.sh',
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
-    before => File_line['check_lowmemory'],
+    before => File_line['check_kernel_leak'],
   }
 
-  file_line { 'check_lowmemory':
+  file_line { 'check_kernel_leak':
     ensure => present,
-    line   => 'command[check_lowmemory]=/usr/lib/nagios/plugins/check_lowmemory.sh -w 3 -c 1',
+    line   => 'command[check_kernel_leak]=/usr/lib/nagios/plugins/check_kernel_leak.sh -w 3,8000000,3 -c 1,10000000,4',
     path   => '/etc/nagios/nrpe_local.cfg',
-    match  => 'command\[check_lowmemory\]',
+    match  => 'command\[check_kernel_leak\]',
     notify => Service['nrpe'],
   }
 
-  @@nagios_service { "check_lowmemory_${::hostname}":
-    check_command       => 'check_nrpe_1arg!check_lowmemory',
+  @@nagios_service { "check_kernel_leak_${::hostname}":
+    check_command       => 'check_nrpe_1arg!check_kernel_leak',
     use                 => 'generic-service-excluding-pagerduty',
     host_name           => $::hostname,
     target              => "/etc/nagios3/conf.d/puppet/service_${::fqdn}.cfg",
-    service_description => "${::hostname}_check_lowmemory",
+    service_description => "${::hostname}_check_kernel_leak",
     tag                 => $::environment,
   }
 
-  @motd::register { 'Nagios Lowmemory Check': }
+  @motd::register { 'Nagios Kernel Leak Check': }
 
 }
