a5517482b951401d5222c81b61bc1f2aa5530525
Ben Field <ben.field@concreteplatform.com>
2015-02-10 18:32:43 +0000

    Disabled pagerduty for load checks in dev

--- manifests/nrpe/load.pp
+++ manifests/nrpe/load.pp
@@ -41,6 +41,13 @@ class nagios::nrpe::load {
 
   $check = "command[check_load]=/usr/lib/nagios/plugins/check_load -w ${loadwarning1},${loadwarning5},${loadwarning15} -c ${loadcritical1},${loadcritical5},${loadcritical15}"
 
+    case $::environment {
+    'production'  : { $service = $nagios_service }
+    'testing'     : { $service = 'generic-service' }
+    'development' : { $service = 'generic-service-excluding-pagerduty' }
+    default       : { $service = 'generic-service-excluding-pagerduty' }
+  }
+
   file_line { 'check_load':
     ensure => present,
     line   => $check,
