7a4bea707e262d7dec9e3bdb930a9cdc2751d87b
Ben Field <ben.field@concreteplatform.com>
2015-05-20 18:29:44 +0100

    Made kernel leak check only run on 32 bit machines

--- manifests/nrpe/kernel_leak.pp
+++ manifests/nrpe/kernel_leak.pp
@@ -4,7 +4,9 @@
 # on 1% AND will warn if more than 8000000 objects, critical on 10000000
 #
 # It will deploy the check, add the command and then create the service on the
-# nagios server
+# nagios server.
+#
+# It will only deploy the check to 32 bit systems as this should not be a problem on 64 bit systems.
 #
 # === Variables
 #
@@ -26,40 +28,41 @@ class nagios::nrpe::kernel_leak {
 
   $monitoring_environment = $::base::params::monitoring_environment
 
-  file { 'check_kernel_leak.sh':
-    ensure => present,
-    path   => '/usr/lib/nagios/plugins/check_kernel_leak.sh',
-    source => 'puppet:///modules/nagios/check_kernel_leak.sh',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-    before => File_line['check_kernel_leak'],
+  if $::architecture == 'i386' or $::architecture == 'x86' {
+    file { 'check_kernel_leak.sh':
+      ensure => present,
+      path   => '/usr/lib/nagios/plugins/check_kernel_leak.sh',
+      source => 'puppet:///modules/nagios/check_kernel_leak.sh',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+      before => File_line['check_kernel_leak'],
+    }
+
+    file_line { 'slabinfo_sudoers':
+      ensure => present,
+      line   => 'nagios ALL=(ALL) NOPASSWD: /bin/cat /proc/slabinfo',
+      path   => '/etc/sudoers',
+      before => File_line['check_kernel_leak'],
+    }
+
+    file_line { 'check_kernel_leak':
+      ensure => present,
+      line   => 'command[check_kernel_leak]=/usr/lib/nagios/plugins/check_kernel_leak.sh -w 2,8000000,3 -c 1,10000000,4',
+      path   => '/etc/nagios/nrpe_local.cfg',
+      match  => 'command\[check_kernel_leak\]',
+      notify => Service['nrpe'],
+    }
+
+    @@nagios_service { "check_kernel_leak_${::hostname}":
+      check_command       => 'check_nrpe_1arg!check_kernel_leak',
+      use                 => 'generic-service-excluding-pagerduty',
+      host_name           => $::hostname,
+      target              => "/etc/nagios3/conf.d/puppet/service_${::fqdn}.cfg",
+      service_description => "${::hostname}_check_kernel_leak",
+      tag                 => $monitoring_environment,
+    }
+
+    @motd::register { 'Nagios Kernel Leak Check': }
   }
-
-  file_line { 'slabinfo_sudoers':
-    ensure => present,
-    line   => 'nagios ALL=(ALL) NOPASSWD: /bin/cat /proc/slabinfo',
-    path   => '/etc/sudoers',
-    before => File_line['check_kernel_leak'],
-  }
-
-  file_line { 'check_kernel_leak':
-    ensure => present,
-    line   => 'command[check_kernel_leak]=/usr/lib/nagios/plugins/check_kernel_leak.sh -w 2,8000000,3 -c 1,10000000,4',
-    path   => '/etc/nagios/nrpe_local.cfg',
-    match  => 'command\[check_kernel_leak\]',
-    notify => Service['nrpe'],
-  }
-
-  @@nagios_service { "check_kernel_leak_${::hostname}":
-    check_command       => 'check_nrpe_1arg!check_kernel_leak',
-    use                 => 'generic-service-excluding-pagerduty',
-    host_name           => $::hostname,
-    target              => "/etc/nagios3/conf.d/puppet/service_${::fqdn}.cfg",
-    service_description => "${::hostname}_check_kernel_leak",
-    tag                 => $monitoring_environment,
-  }
-
-  @motd::register { 'Nagios Kernel Leak Check': }
-
 }
