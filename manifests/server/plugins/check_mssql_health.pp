4edea5587c59001e67904bac960753442d2367e4
Ben Field <ben.field@concreteplatform.com>
2015-10-06 17:36:58 +0100

    Fixed typos

--- manifests/server/plugins/check_mssql_health.pp
+++ manifests/server/plugins/check_mssql_health.pp
@@ -1,9 +1,9 @@
 class nagios::server::plugins::check_mssql_health {
   package { 'libdbd-sybase-perl': ensure => installed, }
 
-  file { 'event_handler.sh':
+  file { 'check_mssql_health':
     ensure => present,
-    path   => '/usr/lib/nagios/eventhandlers/check_mssql_health',
+    path   => '/usr/lib/nagios/plugins/check_mssql_health',
     source => 'puppet:///modules/nagios/server/plugins/check_mssql_health',
     owner  => 'nagios',
     group  => 'nagios',
