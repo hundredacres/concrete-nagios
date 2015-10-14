32b4bc1a0fcaf5f845f57b364b77311f315c4a40
Ben Field <ben.field@concreteplatform.com>
2015-10-14 13:02:41 +0100

    Fixed typo

--- manifests/server/notification/hipchat.pp
+++ manifests/server/notification/hipchat.pp
@@ -46,10 +46,10 @@ class nagios::server::notification::hipchat ($token, $room, $contacts = undef) {
 
   $defaults = {
     ensure => present,
-    service_notification_commands => 'notify_service_by_pagerduty',
+    service_notification_commands => 'notify_service_by_hipchat',
     service_notification_period   => '24x7',
     service_notification_options  => 'c,r',
-    host_notification_commands    => 'notify_host_by_pagerduty',
+    host_notification_commands    => 'notify_host_by_hipchat',
     host_notification_period      => '24x7',
     host_notification_options     => 'd,r',
     email  => '/dev/null',
