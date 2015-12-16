5acbef9da184ced99e017387dd21bac8af2999e9
Ben Field <ben.field@concreteplatform.com>
2015-12-16 17:00:44 +0000

    Changed to use link for centos

--- manifests/folders.pp
+++ manifests/folders.pp
@@ -6,25 +6,41 @@
 #
 # Ben Field <ben.field@concreteplatform.com
 class nagios::folders {
-  file { '/usr/lib/nagios/':
-    ensure  => directory,
-    owner   => 'nagios',
-    group   => 'nagios',
-    mode    => '0755',
+  case $::operatingsystem {
+    'Ubuntu'         : {
+      file { '/usr/lib/nagios/':
+        ensure => directory,
+        owner  => 'nagios',
+        group  => 'nagios',
+        mode   => '0755',
+      }
+    }
+    'RHEL', 'CentOS' : {
+      file { '/usr/lib/nagios/':
+        ensure => link,
+        target => '/usr/lib64/nagios',
+        owner  => 'nagios',
+        group  => 'nagios',
+        mode   => '0755',
+      }
+    }
+    default          : {
+      err('Unsupported OS')
+    }
   }
 
   file { '/usr/lib/nagios/plugins':
-    ensure  => directory,
-    owner   => 'nagios',
-    group   => 'nagios',
-    mode    => '0755',
+    ensure => directory,
+    owner  => 'nagios',
+    group  => 'nagios',
+    mode   => '0755',
   }
 
   file { '/usr/lib/nagios/eventhandlers':
-    ensure  => directory,
-    owner   => 'nagios',
-    group   => 'nagios',
-    mode    => '0755',
+    ensure => directory,
+    owner  => 'nagios',
+    group  => 'nagios',
+    mode   => '0755',
   }
 
 }
